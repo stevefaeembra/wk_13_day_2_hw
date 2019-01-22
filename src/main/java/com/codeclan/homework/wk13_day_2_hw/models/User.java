376fa751bf0588aa84ed5e857b9e644bd956aa57
Steven Kay <stevendkay@gmail.com>
2019-01-22 16:15:43 +0000

    got second test to pass

--- src/main/java/com/codeclan/homework/wk13_day_2_hw/models/User.java
+++ src/main/java/com/codeclan/homework/wk13_day_2_hw/models/User.java
@@ -51,4 +51,8 @@ public class User {
 	public void setFolders(List<Folder> folders) {
 		this.folders = folders;
 	}
+
+	public void addFolder(Folder folder) {
+		this.folders.add(folder);
+	}
 }
--- src/test/java/com/codeclan/homework/wk13_day_2_hw/PersistenceTests.java
+++ src/test/java/com/codeclan/homework/wk13_day_2_hw/PersistenceTests.java
@@ -1,9 +1,11 @@
 package com.codeclan.homework.wk13_day_2_hw;
 
+import com.codeclan.homework.wk13_day_2_hw.models.Folder;
 import com.codeclan.homework.wk13_day_2_hw.models.User;
 import com.codeclan.homework.wk13_day_2_hw.repositories.FileRepository;
 import com.codeclan.homework.wk13_day_2_hw.repositories.FolderRepository;
 import com.codeclan.homework.wk13_day_2_hw.repositories.UserRepository;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +29,26 @@ public class PersistenceTests {
 	@Autowired
 	 UserRepository userRepository;
 
+	@Ignore
 	@Test
 	public void canCreateUser() {
 		userRepository.deleteAll();
 		User user = new User("Joe Bloggs");
 		userRepository.save(user);
 		assertEquals(1, userRepository.count());
+	}
+
+	@Test
+	public void canCreateFolder() {
+		folderRepository.deleteAll();
+		userRepository.deleteAll();
+		User user = new User("Joe Bloggs");
+		Folder folder = new Folder("/users/jbloggs", user);
+		user.addFolder(folder);
+		userRepository.save(user);
+		folderRepository.save(folder);
+		assertEquals(1, folderRepository.count());
+		assertEquals(1, user.getFolders().size());
+		assertEquals(folder, user.getFolders().get(0));
 	}
 }
